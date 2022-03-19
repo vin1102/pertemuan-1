807b4c12ee3a4e8100e105ba6ce5ac1445f3f352
vin1102 <kelvinfebriannugroho@gmail.com>
2022-03-19 17:04:17 +0700

    commit11

--- pertemuan 1.php
+++ pertemuan 1.php
@@ -10,16 +10,62 @@ class handphone {
         $this-> warna = $warna ;
         $this-> spesifikas = $spesifikasi ;
         $this-> harga = $harga ;
+    }
+    public function getlabel () {
+        return "$this->warna, $this->spesifikasi";
+    }
+    public function gettipe () {
+        $str = "{$this->merk} , {$this->getlabel()} , Rp. {$this->harga} ";
+        return $str;
 
     }
-    public function getmerk () {
-        return "$this->merk";
+
+}
+
+class  keyboard {
+    public  $merk, $warna, $jenis, $dimesi, $harga;
+
+    public function __construct ($merk, $warna, $jenis, $dimesi, $harga){
+
+        $this-> merk = $merk ;
+        $this-> warna = $warna ;
+        $this-> jenis = $jenis ;
+        $this-> dimensi = $dimesi ;
+        $this-> harga = $harga ;
+    }
+    public function getlabel () {
+        return "$this->warna, $this->jenis, $this->jenis";
+    }
+    public function gettipe () {
+        $str = "{$this->merk} , {$this->getlabel()} , Rp. {$this->harga} ";
+        return $str;
+    }
+}
+
+class Elektronik extends handphone {
+
+    public function getInfohandphone() {
+        $str = "Elektronik    = {$this->merk} , {$this->getlabel()} , Rp. {$this->harga} ";
+        return $str; 
     }
 
-    }
-    $hp1 = new Handphone("Acer", "Merah", "Snapdragon A7 RAM 4GB", 1400000 );
+}
 
-    echo $hp1->getmerk();
+class CetakInfohandphone {
+
+    public function cetak(handphone $handphone){
+        $str = "{$handphone->merk} {$handphone->getlabel()} (Rp. {$handphone->harga})";
+        return $str;
+    }
+
+ }
+$hp1 = new handphone("Acer", "Merah", "Snapfragon", 3500000);
+
+echo $hp1->getInfohandphone();
+echo "<br>";
+
+
+
 
 
 
