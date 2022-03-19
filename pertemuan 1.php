57774ed7bd0d5ffe43123512c44e1ba034764767
vin1102 <kelvinfebriannugroho@gmail.com>
2022-03-20 00:44:24 +0700

    commit11

--- pertemuan 1.php
+++ pertemuan 1.php
@@ -1,72 +1,150 @@
 <?php
-class handphone {
-    public  $merk, $warna, $spesifikasi, $jenis, $harga;
+class handphone
+{
+    public $merk, $warna, $spesifikasi, $jenis, $harga;
 
-
-    public function __construct ($merk, $warna, $spesifikasi, $harga){
-
-
-        $this-> merk = $merk ;
-        $this-> warna = $warna ;
-        $this-> spesifikas = $spesifikasi ;
-        $this-> harga = $harga ;
+    public function __construct($merk, $warna, $spesifikasi, $harga)
+    {
+        $this->merk = $merk;
+        $this->warna = $warna;
+        $this->spesifikas = $spesifikasi;
+        $this->harga = $harga;
     }
-    public function getlabel () {
+    public function getlabel()
+    {
         return "$this->warna, $this->spesifikasi";
     }
-    public function gettipe () {
-        $str = "{$this->merk} , {$this->getlabel()} , Rp. {$this->harga} ";
-        return $str;
-
-    }
-
-}
-
-class  keyboard {
-    public  $merk, $warna, $jenis, $dimesi, $harga;
-
-    public function __construct ($merk, $warna, $jenis, $dimesi, $harga){
-
-        $this-> merk = $merk ;
-        $this-> warna = $warna ;
-        $this-> jenis = $jenis ;
-        $this-> dimensi = $dimesi ;
-        $this-> harga = $harga ;
-    }
-    public function getlabel () {
-        return "$this->warna, $this->jenis, $this->jenis";
-    }
-    public function gettipe () {
+    public function gettipe()
+    {
         $str = "{$this->merk} , {$this->getlabel()} , Rp. {$this->harga} ";
         return $str;
     }
 }
 
-class Elektronik extends handphone {
+class keyboard
+{
+    public $merk, $warna, $jenis, $dimesi, $harga;
 
-    public function getInfohandphone() {
+    public function __construct($merk, $warna, $jenis, $dimesi, $harga)
+    {
+        $this->merk = $merk;
+        $this->warna = $warna;
+        $this->jenis = $jenis;
+        $this->dimensi = $dimesi;
+        $this->harga = $harga;
+    }
+    public function getlabel()
+    {
+        return "$this->warna, $this->jenis, $this->dimensi";
+    }
+    public function gettipe()
+    {
+        $str = "{$this->merk} , {$this->getlabel()} , Rp. {$this->harga} ";
+        return $str;
+    }
+}
+
+class ElektronikTerbaru extends handphone
+{
+    public function getInfohandphone()
+    {
         $str = "Elektronik    = {$this->merk} , {$this->getlabel()} , Rp. {$this->harga} ";
-        return $str; 
+        return $str;
     }
-
 }
 
-class CetakInfohandphone {
+class ElektronikTerkeren extends handphone
+{
+    public function getInfohandphone()
+    {
+        $str = "Elektronik    = {$this->merk} , {$this->getlabel()} , Rp. {$this->harga} ";
+        return $str;
+    }
+}
 
-    public function cetak(handphone $handphone){
+class Mechanical extends keyboard
+{
+    public function getInfokeyboard()
+    {
+        $str = "keyboard = {$this->merk} , {$this->getlabel()} , Rp. {$this->harga} ";
+        return $str;
+    }
+}
+
+class rgb extends keyboard
+{
+    public function getInfokeyboard()
+    {
+        $str = "keyboard = {$this->merk} , {$this->getlabel()} , Rp. {$this->harga} ";
+        return $str;
+    }
+}
+
+class CetakInfohandphone
+{
+    public function cetak(handphone $handphone)
+    {
         $str = "{$handphone->merk} {$handphone->getlabel()} (Rp. {$handphone->harga})";
         return $str;
     }
+}
 
- }
-$hp1 = new handphone("Acer", "Merah", "Snapfragon", 3500000);
+class CetakInfokeyboard
+{
+    public function cetak(Keyboard $keyboard)
+    {
+        $str = "{$keyboard->merk} {$keyboard->getlabel()} (Rp. {$keyboard->harga})";
+        return $str;
+    }
+}
 
-echo $hp1->getInfohandphone();
+$handphone1 = new ElektronikTerbaru("Acer", "Merah", "Snapdragon 660", 3500000);
+$handphone2 = new ElektronikTerbaru(
+    "Samsung",
+    "maroon",
+    "Mediatek A7",
+    2100000
+);
+$handphone3 = new ElektronikTerkeren(
+    "Xiaomi",
+    "Putih",
+    "Snapdragon 860",
+    4500000
+);
+$handphone4 = new ElektronikTerkeren("Iphone", "Biru", "Binomic A13", 25500000);
+
+$keyboard1 = new Mechanical(
+    "Rexus",
+    "Hitam",
+    "Dilengkapi Dengan RGB",
+    "'14 : 3",
+    500000
+);
+$keyboard2 = new Mechanical("Rudimo", "Hijau", "Mechanical", "'14 : 3", 300000);
+$keyboard3 = new rgb("Banggoda", "Merah", "Mechanical", "'14 : 3", 250000);
+$keyboard4 = new rgb(
+    "Rexus",
+    "Putih",
+    "Dilengkapi Dengan RGB",
+    "'14 : 3",
+    590000
+);
+
+echo $handphone1->getInfohandphone();
 echo "<br>";
+echo $handphone2->getInfohandphone();
+echo "<br>";
+echo $handphone3->getInfohandphone();
+echo "<br>";
+echo $handphone4->getInfohandphone();
+echo "<hr>";
 
-
-
-
-
-
-?> 
+echo $keyboard1->getInfokeyboard();
+echo "<br>";
+echo $keyboard2->getInfokeyboard();
+echo "<br>";
+echo $keyboard3->getInfokeyboard();
+echo "<br>";
+echo $keyboard4->getInfokeyboard();
+echo "<br>";
+?>  ?>
