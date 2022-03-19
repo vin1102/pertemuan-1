ed96333a0cba75c145a5dc8c3139d1f50df1f66c
vin1102 <kelvinfebriannugroho@gmail.com>
2022-03-19 12:13:54 +0700

    commit11

--- pertemuan 1.php
+++ pertemuan 1.php
@@ -1,11 +1,9 @@
+<?php
+class vape  {
+    public  $nama, $merk, $warna, $volt, $jenis, $harga;
 
-askmncnaskcnkaslnc
-siaudcsachuishc
-izhxuhU89HXHa
-sacoisahci9uhs9hc
-sacijh9sahc
-saoichas9uchs9uahc
-sadiasjd9hsa9duh
-saocj0isajcjsiacj
-sacosajci90j
-saichjs89ahc89hsac
+    
+
+
+
+?>
